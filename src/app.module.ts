6a570fd0643df5dbf3f62b32ee1d13c48a4c1b54
Manita Intharachot <64160144@go.buu.ac.th>
2024-09-22 18:14:57 +0700

    Install TypeORM and order

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrderModule } from './order/order.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'test',
+      entities: [],
+      synchronize: true,
+    }),
+    OrderModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
